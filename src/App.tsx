f0cb5d16588e2724838305d7eee72fea8af75896
SebastianPrario <121460546+SebastianPrario@users.noreply.github.com>
2025-02-03 18:07:15 -0300

    fit vercel error

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import GlobalStyles from './GlobalStyles';
-import LoginPage from './pages/Login/Login';
-import SignupPage from './pages/Signup.page';
-import DashBoard from './pages/DashBoard';
-import OrdersView from './pages/OrdersView/OrdersView';
-import { useAuth } from './contexts/AuthContext';
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/Login/Login'
+import SignupPage from './pages/Signup.page'
+import DashBoard from './pages/DashBoard'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import GlobalStyles from './GlobalStyles'
+import OrdersView from './pages/OrdersView/OrdersView'
 
-const App = () => {
-  const { user } = useAuth();
+function App() {
+    const user = localStorage.getItem('token')
 
-  return (
-    <Router>
-      <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={user ? <DashBoard /> : <LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/orders" element={user ? <OrdersView /> : <LoginPage />} />
-      </Routes>
-    </Router>
-  );
-};
+    return (
+        <>
+            <GlobalStyles />
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route
+                    path="/dashboard"
+                    element={user ? <DashBoard /> : <LoginPage />}
+                />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route
+                    path="/orders"
+                    element={user ? <OrdersView /> : <LoginPage />}
+                />
+            </Routes>
+        </>
+    )
+}
 
-export default App;
+export default App
